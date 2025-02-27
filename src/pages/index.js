b103d282f1c76edb20a124d1df48b51059b45df8
Asilbek <a04194950@gmail.com>
2025-02-27 15:27:57 +0500

    login, register

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>
+      <div style={{backgroundColor: "#212526"}}>
         <Header/>
       </div>
     </>
